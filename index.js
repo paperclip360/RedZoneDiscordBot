8b83bd87981923453e4f7e0e6f1e901bc7db11da
paperclip360 <paperclip360@yahoo.com>
2020-07-15 22:22:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token } = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+// need to connect to NFL API www.api.nfl.com 
+
+// need to connect to Yahoo Fantasy api 
+
+// pull list of game ids
+
+// pull players from each of the returned game ids
+
+// match pulled players to Yahoo API league owners
+
+// polling query NFL API for videos based on game id and video type = scoring plays
+
+// post any scoring videos to channel and tag league owner
+
+// need to decide if we want to log data to a database or just write code to read the channel thread in order to avoid duplicate posts
+
+// need to schedule this job to run every x intervals
+
+client.login(token);
